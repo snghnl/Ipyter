a03004f12a30cefcfb6d1f030dff9ffd312e7b62
visitf <viselacity@gmail.com>
2023-01-16 16:53:51 +0900

    flocal.ml

---
+++ PyTER/pycaml/patch.ml
@@ -0,0 +1,13 @@
+(* Patch: Patch Generalization *)
+(* Input: suspicious function F, suspicous line l, suspicious variable x, the result of static anal_(PosType), dynamic anal_(NegType) *)
+(* 1. Prioritizaing templates, using the information from PosType and NegType *)
+(* 2. Initializing templates and tests whether repaired one passes all the test cases *)
+(* 3. If passing all the test cases, return patched program *)
+(* Output: Patched program *)
+
+
+
+
+
+
+
